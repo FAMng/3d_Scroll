1acd08f047964f708f8f13aa6416acc5163c147e
FAMng <filippovam.ng@yandex.ru>
2023-08-22 22:44:48 +0500

    add slider

---
+++ script.js
@@ -0,0 +1,25 @@
+//3D Scroll
+
+let zSpasing = -1000,
+  lastPost = zSpasing / 5,
+  $frames = document.getElementsByClassName("frame"),
+  frames = Array.from($frames),
+  zVal = [];
+
+window.onscroll = function () {
+  let top = document.documentElement.scrollTop,
+    delta = lastPost - top;
+
+  lastPost = top;
+
+  frames.forEach(function (n, i) {
+    zVal.push(i * zSpasing + zSpasing);
+    zVal[i] += delta * -5;
+    let frame = frames[i],
+      transform = `translateZ(${zVal[i]}px)`,
+      opacity = zVal[i] < Math.abs(zSpasing) / 1.2 ? 1 : 0;
+    frame.setAttribute("style", `transform:${transform}`);
+  });
+};
+
+window.scrollTo(0, 1);
